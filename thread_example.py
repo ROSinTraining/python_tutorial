160eee07106e8c2f5cccc629a70ed5bcce8c18c1
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-04-03 13:01:08 +0200

    threading

---
+++ thread_example.py
@@ -0,0 +1,55 @@
+import os
+import time
+from threading import Thread
+
+OddSum = 0
+EvenSum = 0
+
+def findEven(start, end):
+    global EvenSum
+    for x in range(start, end, 1):
+        if((x & 0) == 0):
+            EvenSum += 1
+
+
+def findOdd(start, end):
+    global OddSum
+    for x in range(start, end, 1):
+        if((x & 1) == 0):
+            OddSum += 1
+
+
+if __name__ == '__main__':
+    start = 0
+    end = 19000000
+
+    t1 = Thread(target=findEven, args=(start, end,))
+    t2 = Thread(target=findOdd, args=(start, end,))
+
+    start_time = time.time()
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+    
+    print(" --- Excution time: " + str(time.time() - start_time))
+
+    print("Odd Sum: " + str(OddSum))
+    print("Enven Sum: " + str(EvenSum))
+
+
+# if __name__ == '__main__':
+#     start = 0
+#     end = 190000000
+# 
+#     start_time = time.time()
+# 
+#     findEven(start, end)
+#     findOdd(start, end)
+# 
+#     print(" --- Excution time: " + str(time.time() - start_time))
+# 
+#     print("Odd Sum: " + str(OddSum))
+#     print("Enven Sum: " + str(EvenSum))
